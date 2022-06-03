5053c18a1cc1d4a74240ac0b48e5bb1b6a573725
Jan Laermann <laermannjan@gmail.com>
2022-06-03 02:25:40 +0200

    [broken] fn play

--- src/algorithms/unoptimized.rs
+++ src/algorithms/unoptimized.rs
@@ -1,10 +1,21 @@
-use crate::{Guess, Guesser};
+use std::collections::HashMap;
 
-pub struct Unoptimized;
+use crate::{Guess, Guesser, DICTIONARY};
+
+pub struct Unoptimized {
+    remaining: HashMap<&'static str, usize>,
+}
 
 impl Unoptimized {
     pub fn new() -> Self {
-        Unoptimized
+        Unoptimized {
+            remaining: HashMap::from_iter(DICTIONARY.lines().map(|line| {
+                let (word, count) = line.split_once(' ')
+                    .expect("every line is word + space + frequency");
+                let count: usize = count.parse().expect("every count is a number");
+                (word, count)
+            })), 
+        }
     }
 }
 
